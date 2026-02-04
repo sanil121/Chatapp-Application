53be84326f0fdcce5e625e6c53c2a11db37ef428
SanilGhosh <sanilg245@gmail.com>
2026-02-04 18:55:35 +0530

    fornt end complete

---
+++ frontend/src/context/SocketContext.jsx
@@ -0,0 +1,42 @@
+import { createContext, useState, useEffect, useContext } from "react";
+import { useAuthContext } from "./AuthContext";
+import io from "socket.io-client";
+
+const SocketContext = createContext();
+
+export const useSocketContext = () => useContext(SocketContext);
+
+export const SocketContextProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const { authUser } = useAuthContext();
+
+  useEffect(() => {
+    if (authUser) {
+      const newSocket = io("https://chatapp-kaushlendra.onrender.com", {
+        query: {
+          userId: authUser._id,
+        },
+      });
+
+      setSocket(newSocket);
+
+      newSocket.on("getOnlineUsers", (users) => {
+        setOnlineUsers(users);
+      });
+
+      return () => newSocket.close();
+    } else {
+      if (socket) {
+        socket.close();
+        setSocket(null);
+      }
+    }
+  }, [authUser]);
+
+  return (
+    <SocketContext.Provider value={{ socket, onlineUsers }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
